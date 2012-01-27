76020082a485b370f86c34841c473698486f27d9
Evgeny Novoseltsev <evgeny_novoseltsev@web.de>
2012-01-27 17:44:46 +0100

    LazyTree.hs changed

--- LazyTree.hs
+++ LazyTree.hs
@@ -1,4 +1,4 @@
-module LazyTreeExt where
+module LazyTree where
 
 import Data.List
 
@@ -13,42 +13,46 @@ suffixes []       = []
 suffixes aw@(_:w) = aw:suffixes w 
 
 select :: (Eq alph) => [Word alph] -> alph -> [Word alph]
-select ss a = [c:u | c:u <- ss, a == c]
+select ss a = [u | c:u <- ss, length u > 0, a == c]
 
 edge_cst :: (Eq alph) => EdgeFunction alph
-edge_cst [s] = (s, [[]])
-edge_cst awss@((a:_):ss) | all (\(c:_) -> a == c) ss =  (a:cp, rss)
-                         | otherwise               =  ([], awss)
-                           where (cp, rss)         =  edge_cst ([t | _:t <- awss])
+edge_cst []     = ([], [])
+edge_cst [s]    = (s, [[]])
+edge_cst ([]:w) = ([], w)
+edge_cst awss@((a:_):ss)
+    | all p ss  = (a:cp, rss)
+    | otherwise = ([], awss)
+        where p []    = False
+              p (c:_) = a == c
+              (cp, rss) = edge_cst ([t | _:t <- awss])
 
 lazy_cst :: (Eq alph) => (EdgeFunction alph) -> Word alph -> Word alph -> STree alph
 lazy_cst edge_cst alphabet t = sTr (suffixes t) 
     where sTr [[]] = Leaf 
-          sTr ss   = Branch[(cp,sTr rss)| a <- alphabet, let gs = select ss a, let (cp,rss) = edge_cst gs]
+          sTr ss   = Branch[(a:cp, sTr rss)| a <- alphabet, let gs = select ss a, length gs > 0, let (cp, rss) = edge_cst gs]
 
-is_same_firstLetter :: (Eq alph) => [Word alph] -> Bool
-is_same_firstLetter [] = True
-is_same_firstLetter [w] = True
-is_same_firstLetter ((x:xs):(y:ys):xss) = (x==y) && (is_same_firstLetter ((x:xs):xss))
-
-common_prefix :: (Eq alph) => [Word alph] -> Word alph
-
-common_prefix ws = common_prefix_aux ws []
-    where common_prefix_aux [] aux = reverse aux
-          common_prefix_aux ws@((x:xs):xxs) aux | is_same_firstLetter ((x:xs):xxs) = common_prefix_aux ([ys | _:ys <- ws]) (x:aux)
-                                                | otherwise                        = reverse aux
-
-instance Show (STree alph) where
-    show t = "" 
+printTree :: (Show alf) => Int -> STree alf -> IO ()
+printTree i Leaf        = return ()
+printTree i (Branch es) = g i es
+    where
+        g i ([])          = return ()
+        g i ((w, st):lst) = do putStr $ replicate i ' ' ++ "+"
+                               print w
+                               printTree (i + 1) st
+                               g i lst
 
 main = 
-    let alphabet = ["a","b","c","d","e","f","g"]
-        word = ["a","g","c","g","a","c","g","a","g"]
-    in  lazy_cst edge_cst alphabet word
+    let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+        word = "agcgacgag"
+    in  printTree 0 $ lazy_cst edge_cst alphabet word
+
                                                 
 --search :: (Eq alph) => Word alph -> STree alph -> Bool
---search w Leaf = False
---search w1 (Branch[(w2, st):lst]) =  if common_prefix (w1:w2:[]) == [] then search w1 lst
---                                    else if common_prefix (w1:w2:[]) == w2 then search (stripPrefix w2 w1) st
---                                    else if common_prefix (w1:w2:[]) == w1 then True
+--search w (Leaf)                  = False
+--search w1 (Branch((w2, st):lst)) =  case g w1 ((w2, st):lst)) of 
+--                                         ([],_) -> g w1 lst
+--                                         (w2, v1:_) 
+--                                    else if commonPrefix (w1:w2:[]) == w2 then search (stripPrefix w2 w1) st
+--                                    else if commonPrefix (w1:w2:[]) == w1 then True
 --                                    else False
+--                                    where g w1 ((w2, st):lst)) = commonPrefix (w1:w2:[])
